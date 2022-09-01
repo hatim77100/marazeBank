3ad5227c6925365c0164fe0a3c675b886e57195a
hatim77100 <najoua-hatim@hotmail.fr>
2022-09-01 23:55:31 +0200

    add function add & to substract

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import '@fortawesome/fontawesome-free/js/all';
+import "@fortawesome/fontawesome-free/js/all";
 // import _ from 'lodash';
 // import './style.css';
 // import Icon from './icon.png/opti.png';
 
 // function component() {
 //     const element = document.createElement('div');
-  
+
 //     // Lodash, currently included via a script, is required for this line to work
 //     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 //     element.classList.add('hello');
@@ -16,21 +16,44 @@ import '@fortawesome/fontawesome-free/js/all';
 //   element.appendChild(myIcon);
 //     return element;
 //   }
-  
+
 //   document.body.appendChild(component());
 
 // console.log("script ok");
+const btnDeposer = document.getElementById("btn-deposer");
+const inputDeposer = document.getElementById("deposer");
+const btnRetirer = document.getElementById("btn-retirer");
+const inputRetirer = document.getElementById("retirer");
+const autorisation = document.getElementById("autorisation");
+const monSolde = document.getElementById("solde");
 
-let btn1 = document.getElementById("btn1");
-let inp1 = document.getElementById("inp1").value;
-let span2 = document.querySelector('#span2');
-console.log(inp1);
-// btn1.addEventListener("click", ()=> {
-//     span2.innerHTML = inp1;
-// });
-
+// console.log(inputRetirer);
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  return Math.floor(Math.random() * (max + min));
 }
-  
-span2.innerHTML = getRndInteger(-300, 100000);
+
+// autorisation de découvert random:
+let decouverte = getRndInteger(-600, 0);
+autorisation.append(decouverte);
+
+// Le solde random:
+let solde = getRndInteger(-300, 10000);
+monSolde.append(solde);
+
+btnDeposer.addEventListener("click", () => {
+  let valeur1 = parseInt(inputDeposer.value);
+  let valeur2 = parseInt(solde);
+  let result = valeur1 + valeur2;
+  monSolde.textContent = result;
+});
+
+btnRetirer.addEventListener("click", () => {
+    if(decouverte >= -600){
+        let valeur1 = parseInt(inputRetirer.value);
+        let valeur2 = parseInt(solde);
+        let result = valeur2 - valeur1;
+        monSolde.textContent = result;
+    } else {
+        
+    }
+});
