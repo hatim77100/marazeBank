70fd770ac4f2f402cba14bd1feeb8edb9e57446d
hatim77100 <najoua-hatim@hotmail.fr>
2022-09-02 01:02:00 +0200

    update

--- src/index.js
+++ src/index.js
@@ -26,20 +26,23 @@ const btnRetirer = document.getElementById("btn-retirer");
 const inputRetirer = document.getElementById("retirer");
 const autorisation = document.getElementById("autorisation");
 const monSolde = document.getElementById("solde");
+const decouvertDepasser = document.getElementById("decouvert-depassé");
 
-// console.log(inputRetirer);
+// console.log(decouvertDepasser);
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max + min));
 }
 
-// autorisation de découvert random:
-let decouverte = getRndInteger(-600, 0);
-autorisation.append(decouverte);
+// autorisation de découvert:
+let decouvert = 300;
+autorisation.append(decouvert);
 
 // Le solde random:
-let solde = getRndInteger(-300, 10000);
+// let solde = getRndInteger(-300, 10000);
+let solde = 0;
 monSolde.append(solde);
 
+// Event
 btnDeposer.addEventListener("click", () => {
   let valeur1 = parseInt(inputDeposer.value);
   let valeur2 = parseInt(solde);
@@ -48,12 +51,13 @@ btnDeposer.addEventListener("click", () => {
 });
 
 btnRetirer.addEventListener("click", () => {
-    if(decouverte >= -600){
-        let valeur1 = parseInt(inputRetirer.value);
-        let valeur2 = parseInt(solde);
-        let result = valeur2 - valeur1;
-        monSolde.textContent = result;
-    } else {
-        
-    }
+  if (inputRetirer.value > decouvert & solde <= 0 ) {
+    decouvertDepasser.textContent ="vous nous pouvez pas retirer de l'argent";
+    console.log(inputRetirer.value);
+  } else {
+    let valeur1 = parseInt(inputRetirer.value);
+    let valeur2 = parseInt(solde);
+    let result = valeur2 - valeur1;
+    monSolde.textContent = result;
+  }
 });
