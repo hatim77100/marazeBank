2ed262287b9b0383cb99f421a91dc5155874b834
hatim77100 <najoua-hatim@hotmail.fr>
2022-09-02 19:52:10 +0200

    update users

---
+++ src/components/profils.js
@@ -0,0 +1,11 @@
+class Users {
+  constructor(numeroDeCompte, iban, codeBanque, codeGuichet, cleRib) {
+    this.numeroDeCompte = numeroDeCompte;
+    this.iban = iban;
+    this.codeBanque = codeBanque;
+    this.codeGuichet = codeGuichet;
+    this.cleRib = cleRib;
+  }
+}
+
+export default Users;
--- src/index.js
+++ src/index.js
@@ -20,12 +20,32 @@ import "@fortawesome/fontawesome-free/js/all";
 //   document.body.appendChild(component());
 
 // console.log("script ok");
+
+// users
+import Users from "./components/profils";
+
+const numeroDeCompte = document.getElementById("numero-compte");
+const iban = document.getElementById("iban");
+const codeBanque = document.getElementById("code-banque");
+const codeGuichet = document.getElementById("code-guichet");
+const cleRib = document.getElementById("clé-rib");
+
+let profil1 = new Users(156897895959, "FR15895612558", 25894, 20015, 56);
+
+numeroDeCompte.textContent = profil1.numeroDeCompte;
+iban.textContent = profil1.iban;
+codeBanque.textContent = profil1.codeBanque;
+codeGuichet.textContent = profil1.codeGuichet;
+cleRib.textContent = profil1.cleRib;
+
+// 
+
 const btnDeposer = document.getElementById("btn-deposer");
 const inputDeposer = document.getElementById("deposer");
 const btnRetirer = document.getElementById("btn-retirer");
 const inputRetirer = document.getElementById("retirer");
 const autorisation = document.getElementById("autorisation");
-const monSolde = document.getElementById("solde");
+const inputSolde = document.getElementById("solde");
 const decouvertDepasser = document.getElementById("decouvert-depassé");
 
 // console.log(decouvertDepasser);
@@ -35,29 +55,45 @@ function getRndInteger(min, max) {
 
 // autorisation de découvert:
 let decouvert = 300;
-autorisation.append(decouvert);
+autorisation.textContent = decouvert;
 
 // Le solde random:
 // let solde = getRndInteger(-300, 10000);
-let solde = 0;
-monSolde.append(solde);
+let Solde = 0;
 
-// Event
-btnDeposer.addEventListener("click", () => {
+inputSolde.textContent = Solde;
+console.log(inputSolde);
+
+// Events
+btnDeposer.addEventListener("click", (valeur1) => {
   let valeur1 = parseInt(inputDeposer.value);
-  let valeur2 = parseInt(solde);
-  let result = valeur1 + valeur2;
-  monSolde.textContent = result;
+  return Solde += valeur1;
+  // let valeur2 = parseInt(inputSolde.value);
+  // let result = valeur1 + valeur2;
+  inputSolde.textContent = Solde;
 });
 
 btnRetirer.addEventListener("click", () => {
-  if (inputRetirer.value > decouvert & solde <= 0 ) {
-    decouvertDepasser.textContent ="vous nous pouvez pas retirer de l'argent";
-    console.log(inputRetirer.value);
-  } else {
+  // if (inputRetirer.value > decouvert & solde <= 0 ) {
+  //   decouvertDepasser.textContent ="vous nous pouvez pas retirer de l'argent";
+  //   console.log(inputRetirer.value);
+  // } else {
     let valeur1 = parseInt(inputRetirer.value);
-    let valeur2 = parseInt(solde);
+    let valeur2 = parseInt(inputSolde.value);
     let result = valeur2 - valeur1;
-    monSolde.textContent = result;
-  }
+    inputSolde.textContent = result;
+  // }
 });
+
+
+// exercice
+let sold = 0
+const add = (value) => {
+  return sold += value;
+}
+
+console.log(add());
+console.log(sold);
+
+console.log(add());
+console.log(sold);
