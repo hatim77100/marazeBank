15c3af3c61615c7985a4953af5fe77331ce9d426
hatim77100 <najoua-hatim@hotmail.fr>
2022-09-07 23:40:39 +0200

    update arithmetic

--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
 import "@fortawesome/fontawesome-free/js/all";
-// import _ from 'lodash';
-// import './style.css';
-// import Icon from './icon.png/opti.png';
-
-// function component() {
-//     const element = document.createElement('div');
-
-//     // Lodash, currently included via a script, is required for this line to work
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//     element.classList.add('hello');
-//      // Add the image to our existing div.
-//   const myIcon = new Image();
-//   myIcon.src = Icon;
-
-//   element.appendChild(myIcon);
-//     return element;
-//   }
-
-//   document.body.appendChild(component());
-
-// console.log("script ok");
 
 // users
 import Users from "./components/profils";
@@ -38,7 +17,7 @@ codeBanque.textContent = profil1.codeBanque;
 codeGuichet.textContent = profil1.codeGuichet;
 cleRib.textContent = profil1.cleRib;
 
-// 
+//
 
 const btnDeposer = document.getElementById("btn-deposer");
 const inputDeposer = document.getElementById("deposer");
@@ -59,41 +38,24 @@ autorisation.textContent = decouvert;
 
 // Le solde random:
 // let solde = getRndInteger(-300, 10000);
-let Solde = 0;
+let solde = 0;
 
-inputSolde.textContent = Solde;
+inputSolde.textContent = solde;
 console.log(inputSolde);
 
 // Events
-btnDeposer.addEventListener("click", (valeur1) => {
-  let valeur1 = parseInt(inputDeposer.value);
-  return Solde += valeur1;
-  // let valeur2 = parseInt(inputSolde.value);
-  // let result = valeur1 + valeur2;
-  inputSolde.textContent = Solde;
+btnDeposer.addEventListener("click", () => {
+  let resultat = parseInt(inputDeposer.value);
+       solde+=resultat;
+  inputSolde.textContent = solde;
 });
 
 btnRetirer.addEventListener("click", () => {
-  // if (inputRetirer.value > decouvert & solde <= 0 ) {
-  //   decouvertDepasser.textContent ="vous nous pouvez pas retirer de l'argent";
-  //   console.log(inputRetirer.value);
-  // } else {
-    let valeur1 = parseInt(inputRetirer.value);
-    let valeur2 = parseInt(inputSolde.value);
-    let result = valeur2 - valeur1;
-    inputSolde.textContent = result;
-  // }
+  let valeur1 = parseInt(inputRetirer.value);
+      solde-= valeur1;
+  inputSolde.textContent = solde;
+ 
 });
 
 
-// exercice
-let sold = 0
-const add = (value) => {
-  return sold += value;
-}
 
-console.log(add());
-console.log(sold);
-
-console.log(add());
-console.log(sold);
